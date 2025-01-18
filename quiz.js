c83f4a84e8d54a9e6368738f3a32b0142be9a69e
rgyslain <gislainrugira@gmail.com>
2025-01-18 14:56:10 +0200

    wee

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer() {
+   let correctAnswer ="4";
+ const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+ function comparison() {
+    if (correctAnswer==userAnswer) {
+        document.getElementById('feedback').textContent="Correct! Well done.";
+        
+    } else {
+        document.getElementById('feedback').textContent="That's incorrect. Try again!";
+
+
+        
+    }
+    document.getElementById('submit-answer').addEventListener('click', checkAnswer);
+    
+ }
+
+}
