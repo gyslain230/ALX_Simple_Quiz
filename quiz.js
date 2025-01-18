23ad6b2ca00842c76d0bae9cdccd69a5b1a4441d
rgyslain <gislainrugira@gmail.com>
2025-01-18 15:04:55 +0200

    h

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer() {
    let correctAnswer ="4";
  const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
  function comparison() {
-    if (correctAnswer === userAnswer) {
+    if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent="Correct! Well done.";
         
     } else {
